d28164a70fbb01ccb564b9b710a86539b5610ae2
Usman Jamshed <usman.jamshedk@gmail.com>
2022-09-04 22:15:50 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+with open('README.md') as f:
+    long_description= f.read()
 
 setup(name='dgbg',
 version='0.0.1',
 description='Docking grid box generator for Autodock4 and Vina',
-long_description=readme(),
-long_description_type= 'text/markdown',
+long_description=long_description,
+long_description_content_type= 'text/markdown',
 classifiers=[
     'Development Status :: 5 - Production/Stable',
     'License :: OSI Approved :: MIT License',
@@ -23,4 +22,5 @@ license='MIT',
 packages=['dgbg'],
 install_requires=['pandas', 'numpy', 'mdtraj', 'nglview'],
 include_package_data=True,
-zip_safe=False)
+zip_safe=False,
+download_url="https://github.com/ujamshed/dgbg/archive/refs/tags/0.0.1.tar.gz")
